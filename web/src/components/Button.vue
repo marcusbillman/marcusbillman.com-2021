eaf0e9d151f125885ad08346f5c1ff4c4da4acb4
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-04-17 18:44:25 +0200

    💄 Button: Hover effect

--- web/src/components/Button.vue
+++ web/src/components/Button.vue
@@ -1,7 +1,9 @@
 <template>
   <a class="button" :href="href">
-    <slot></slot>
-    <i class="gg-arrow-top-right"></i>
+    <div class="button__inner">
+      <slot></slot>
+      <i class="gg-arrow-top-right"></i>
+    </div>
   </a>
 </template>
 
@@ -16,23 +18,47 @@ defineProps(["href"]);
 
 .button {
   width: 100%;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  color: $white;
-  background: $blueberry-500;
+  position: relative;
+  display: inline-block;
   font-size: 1.6rem;
   font-weight: 500;
   text-decoration: none;
-  border-radius: 0.8rem;
-  padding: 1.4rem 2.4rem;
-  i {
-    transform: translateY(-2px);
-    margin-left: 1.6rem;
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: $blueberry-50;
+    border-radius: 0.8rem;
+    z-index: -1;
   }
-
+  &:hover {
+    .button__inner {
+      color: $blueberry-500;
+      background: transparent;
+      transform: translate(-8px, -8px);
+    }
+  }
   @media screen and (min-width: 40rem) {
     width: auto;
+  }
+  &__inner {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: $white;
+    background: $blueberry-500;
+    border: 3px solid $blueberry-500;
+    border-radius: 0.8rem;
+    padding: 1.4rem 2.4rem;
+    transition: all 0.1s;
+    i {
+      transform: translateY(-2px);
+      margin-left: 1.6rem;
+    }
   }
 }
 </style>
