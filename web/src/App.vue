34fb80415ed7d4bf76aac90b3d7daacf9f5520d7
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-04-18 16:57:25 +0200

    ✨ App: Add router-view

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <Home />
+  <router-view v-slot="{ Component }">
+    <keep-alive>
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
 </template>
 
 <script setup>
