e18230f6c58c20fb5e0a742b1523d80d53fef10c
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-04-20 10:34:52 +0200

    🔧 Add sanityConfig.js

---
+++ web/src/sanityConfig.js
@@ -0,0 +1,8 @@
+import sanityClient from "@sanity/client";
+
+export default sanityClient({
+  projectId: "6m918bwg",
+  dataset: "production",
+  apiVersion: "2021-04-20",
+  useCdn: process.env.NODE_ENV === "production",
+});
