b66ab78020dcd8a924f07b9a94af6d444373b2e5
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-04-20 11:23:53 +0200

    ✨ Portfolio: Display ProjectCard components

--- web/src/views/Portfolio.vue
+++ web/src/views/Portfolio.vue
@@ -1,5 +1,15 @@
-<template></template>
+<template>
+  <div class="container">
+    <ProjectCard
+      v-for="project in $store.state.projects"
+      :key="project._key"
+      :project="project"
+    />
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+import ProjectCard from "@/components/ProjectCard.vue";
+</script>
 
 <style lang="scss" scoped></style>
