9082f079cbf194f758f88dd7f39928e248336921
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-04-20 11:22:59 +0200

    ✨ ProjectCard component

---
+++ web/src/components/ProjectCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="project">
+    <img :src="urlFor(project.mainImage)" :alt="project.title" />
+    <p>{{ project.title }}</p>
+    <p v-for="roleTag in project.roleTags" :key="roleTag._key">{{ roleTag }}</p>
+  </article>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import sanityClient from "@/sanityConfig";
+import imageUrlBuilder from "@sanity/image-url";
+
+defineProps(["project"]);
+
+const imageBuilder = imageUrlBuilder(sanityClient);
+
+function urlFor(source) {
+  return imageBuilder.image(source);
+}
+</script>
+
+<style lang="scss" scoped></style>
