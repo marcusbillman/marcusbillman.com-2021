3b53bccb60515e575275290d97d485b2b4110195
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-04-23 09:29:05 +0200

    💄 ProjectCard: Add styles

--- web/src/components/ProjectCard.vue
+++ web/src/components/ProjectCard.vue
@@ -5,18 +5,18 @@
       :src="urlFor(project.mainImage)"
       :alt="project.title"
     />
-    <div class="project__info">
+    <div class="flow project__info">
       <router-link class="project__link" to="/portfolio">
         <h3 class="project__title">{{ project.title }}</h3>
         <i class="gg-arrow-right"></i>
       </router-link>
       <ul class="project__roles">
-        <li class="project__role">
-          <Tag
-            v-for="roleTag in project.roleTags"
-            :key="roleTag._key"
-            :text="roleTag.name"
-          />
+        <li
+          class="project__role"
+          v-for="roleTag in project.roleTags"
+          :key="roleTag._key"
+        >
+          <Tag :text="roleTag.name" />
         </li>
       </ul>
     </div>
@@ -38,4 +38,59 @@ function urlFor(source) {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "@/styles/breakpoints" as *;
+@use "@/styles/colours" as *;
+
+.project {
+  position: relative;
+  &__image {
+    width: 100%;
+    max-width: 100%;
+    border-radius: 1.6rem;
+    margin-bottom: 2.4rem;
+  }
+  &__info {
+    min-width: 80%;
+    @include for-tablet-landscape-up {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      transform: translate(-3.2rem, 3.2rem);
+      background: $white;
+      border-radius: 1.6rem;
+      border: 2px solid $grey-200;
+      padding: 3.2rem;
+    }
+  }
+  &__link {
+    display: flex;
+    align-items: center;
+    color: $blueberry-500;
+    text-decoration: none;
+  }
+  &__title {
+    font-size: 2.4rem;
+    font-weight: 500;
+    text-transform: unset;
+    letter-spacing: unset;
+    margin-right: 0.8rem;
+    @include for-tablet-landscape-up {
+      font-size: 3.2rem;
+    }
+  }
+  i {
+    @include for-tablet-landscape-up {
+      --ggs: calc(32 / 22);
+      margin-left: 1.2rem;
+    }
+  }
+  &__roles {
+    display: flex;
+    flex-wrap: wrap;
+    * {
+      margin-right: 1rem;
+    }
+  }
+}
+</style>
