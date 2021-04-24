ee71f9a5d2e73f583c8519702f29b8d4a830dbf7
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-04-24 19:14:32 +0200

    ♻️ BigLink: Use new Icon component

--- web/src/components/BigLink.vue
+++ web/src/components/BigLink.vue
@@ -1,11 +1,12 @@
 <template>
   <a class="big-link" :href="href">
     <slot></slot>
-    <i :class="'gg-' + icon"></i>
+    <Icon :name="icon" />
   </a>
 </template>
 
 <script setup>
+import Icon from "@/components/Icon.vue";
 import { defineProps } from "vue";
 
 defineProps(["href", "icon"]);
@@ -21,11 +22,11 @@ defineProps(["href", "icon"]);
   @include for-desktop-up {
     font-size: 4rem;
   }
-  i {
+  .icon {
     margin-left: 0.8rem;
     @include for-desktop-up {
+      transform: scale(calc(40 / 24));
       margin-left: 2.4rem;
-      --ggs: calc(40 / 22);
     }
   }
 }
