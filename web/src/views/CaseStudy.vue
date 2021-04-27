2c5c6ccfe31d806b2f1a48e9ab518a4f7af6e8a7
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-04-27 19:40:17 +0200

    ✨ CaseStudy: Get project from store based on slug

--- web/src/views/CaseStudy.vue
+++ web/src/views/CaseStudy.vue
@@ -1,32 +1,19 @@
 <template>
-  <h1>Case Study page</h1>
-  <p>Very nice {{ $store.state.projects }}</p>
-  <p>{{ project }}</p>
+  <div v-if="project">
+    <h1>{{ project.title }}</h1>
+  </div>
 </template>
 
 <script setup>
-import { ref, watch } from "vue";
+import { computed } from "vue";
 import { useRoute } from "vue-router";
 import { useStore } from "vuex";
 
 const route = useRoute();
 const store = useStore();
 
-// const project = ref({});
-
-setTimeout(async () => {
-  console.log(await store.getters.getProjectBySlug("lorem-ipsum"));
-}, 1000);
-
-const unsubscribe = store.subscribe((mutation, state) => {
-  console.log(mutation.type);
-  console.log(mutation.payload);
-});
-
-unsubscribe();
-
-// const shit = await store.getters.getProjectBySlug("lorem-ipsum");
-// project.value = await store.getters.getProjectBySlug("lorem-ipsum");
+const slug = route.params.slug;
+const project = computed(() => store.getters.getProjectBySlug(slug));
 </script>
 
 <style lang="scss" scoped></style>
