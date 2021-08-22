8628238bcfe9fae78f44211ff7efedaca0fc4a23
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-08-22 19:34:13 +0200

    ✨ ProjectList: Create component

---
+++ web/src/components/ProjectList.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="projects">
+    <ProjectCard
+      v-for="(project, index) in $store.state.projects"
+      :key="project._key"
+      :project="project"
+      :info-side="index % 2 === 0 ? 'left' : 'right'"
+      :compact="compact"
+    />
+  </section>
+</template>
+
+<script setup>
+import ProjectCard from '@/components/ProjectCard.vue'
+import { defineProps } from 'vue'
+
+defineProps(['compact'])
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/breakpoints" as *;
+@use "@/styles/colours" as *;
+@use "@/styles/lines" as *;
+
+.projects {
+  display: grid;
+  grid-template-columns: repeat(1, minmax(20rem, 1fr));
+  gap: 6.4rem;
+  @include for-tablet-landscape-up {
+    row-gap: clamp(6.4rem, 20rem, 10vw);
+    padding: 0 clamp(3.2rem, 40rem, 10vw - 2.4rem);
+  }
+  @include for-desktop-up {
+    grid-template-columns: repeat(2, minmax(20rem, 1fr));
+    .project {
+      &:nth-child(even) {
+        transform: translateY(50%);
+      }
+      &:last-child {
+        margin-bottom: 50%;
+      }
+    }
+  }
+}
+</style>
