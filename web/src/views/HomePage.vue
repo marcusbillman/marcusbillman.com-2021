8eb92241fc8892d81fe19e7a574411f44ddcba59
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-08-23 21:24:34 +0200

    💄 HomePage: Add decorative ruler border

--- web/src/views/HomePage.vue
+++ web/src/views/HomePage.vue
@@ -4,15 +4,22 @@
       <HeroSection />
       <div class="sections">
         <section class="bio section">
-          <div class="bio__content container">
-            <p class="bio__body">
-              I’m Marcus. I design cohesive, helpful and accessible web
-              experiences that simply feel great. I’m also a hobbyist music
-              producer.
-            </p>
-            <BaseButton href="/about" variant="secondary" icon="arrow-right"
-              >About me</BaseButton
-            >
+          <div class="bio__container container">
+            <div class="bio__content">
+              <p class="bio__body">
+                I’m Marcus. I design cohesive, helpful and accessible web
+                experiences that simply feel great. I’m also a hobbyist music
+                producer.
+              </p>
+              <BaseButton href="/about" variant="secondary" icon="arrow-right"
+                >About me</BaseButton
+              >
+            </div>
+            <img
+              v-svg-inline
+              class="bio__ruler-border"
+              src="@/assets/home-ruler-border.svg"
+            />
           </div>
         </section>
         <section class="featured-work section">
@@ -71,18 +78,39 @@ import ProjectList from '@/components/ProjectList.vue'
 }
 
 .bio {
+  &__container {
+    position: relative;
+    max-width: calc(100rem + 6.4rem);
+    z-index: 1;
+    @include for-tablet-landscape-up {
+      padding: 0;
+    }
+  }
   &__content {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     gap: 3.2rem;
-    max-width: 106.4rem;
-  }
-  @include for-tablet-landscape-up {
-    &__content {
+    background: $white;
+    @include for-tablet-landscape-up {
       flex-direction: row;
       align-items: center;
       padding: 3.2rem;
+    }
+  }
+  &__ruler-border {
+    display: none;
+    position: absolute;
+    left: 50%;
+    bottom: 50%;
+    transform: translateX(-50%);
+    width: 120rem;
+    height: calc(100vh + 100%);
+    overflow: visible;
+    z-index: -1;
+    pointer-events: none;
+    @include for-desktop-up {
+      display: block;
     }
   }
 }
