e9ff791d7bbfcb94d000ffbcc2d5c1cd3411630d
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-08-23 22:50:33 +0200

    ✨ ProjectList: Implement featured-only mode

--- web/src/components/ProjectList.vue
+++ web/src/components/ProjectList.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="projects">
     <ProjectListCard
-      v-for="(project, index) in $store.state.projects"
+      v-for="(project, index) in projectsToShow"
       :key="project._key"
       :project="project"
       :info-side="index % 2 === 0 ? 'left' : 'right'"
@@ -12,15 +12,21 @@
 
 <script setup>
 import ProjectListCard from '@/components/ProjectListCard.vue'
-import { defineProps } from 'vue'
+import { computed, defineProps, toRefs } from 'vue'
+import { useStore } from 'vuex'
 
-defineProps(['compact'])
+const store = useStore()
+const props = defineProps(['compact', 'featuredOnly'])
+
+// Read the featuredOnly prop and decide what projects to show
+const { featuredOnly } = toRefs(props)
+const projectsToShow = computed(() =>
+  featuredOnly.value ? store.getters.getFeaturedProjects : store.state.projects
+)
 </script>
 
 <style lang="scss" scoped>
 @use "@/styles/breakpoints" as *;
-@use "@/styles/colours" as *;
-@use "@/styles/lines" as *;
 
 .projects {
   display: grid;
