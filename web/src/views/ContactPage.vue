617cf8e68e623d24d977a8785a4ad5abc799f86c
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-08-24 21:33:50 +0200

    💄 ContactPage: Update to new design

--- web/src/views/ContactPage.vue
+++ web/src/views/ContactPage.vue
@@ -1,53 +1,38 @@
 <template>
   <div class="page">
+    <TheHeader title="Contact" />
     <div class="container">
-      <h1>Contact</h1>
-      <div class="contact">
-        <div>
-          <p>
-            You can leave me a message through this fancy form. Or if you
-            prefer, you may also
-            <a href="mailto:hello@marcusbillman.com">email me directly.</a>
-          </p>
-          <form class="form" method="post" name="contact" netlify>
-            <input type="hidden" name="form-name" value="contact" />
-            <div class="form__fields">
-              <div class="form__field">
-                <label for="name">Your name</label>
-                <input id="name" type="text" name="name" />
+      <div class="sections">
+        <section class="contact-methods section">
+          <div class="contact-methods__email-wrapper">
+            <p class="contact-methods__heading">
+              Hej! You can email me directly...
+            </p>
+            <EmailCard />
+          </div>
+          <div class="contact-methods__form-wrapper">
+            <p class="contact-methods__heading">...or use this nifty form.</p>
+            <form class="form" method="post" name="contact" netlify>
+              <input type="hidden" name="form-name" value="contact" />
+              <div class="form__fields">
+                <div class="form__field">
+                  <label for="name">Your name</label>
+                  <input id="name" type="text" name="name" />
+                </div>
+                <div class="form__field">
+                  <label for="email">Your email</label>
+                  <input id="email" type="email" name="email" />
+                </div>
+                <div class="form__field">
+                  <label for="message">Message</label>
+                  <textarea id="message" name="message"></textarea>
+                </div>
               </div>
-              <div class="form__field">
-                <label for="email">Your email</label>
-                <input id="email" type="email" name="email" />
-              </div>
-              <div class="form__field">
-                <label for="message">Message</label>
-                <textarea id="message" name="message"></textarea>
-              </div>
-            </div>
-            <BaseButton type="submit">Send message</BaseButton>
-          </form>
-        </div>
-        <section class="socials">
-          <LinkCard
-            title="GitHub"
-            desc="marcusbillman"
-            href="https://github.com/marcusbillman"
-            icon="code-slash"
-          ></LinkCard>
-          <LinkCard
-            title="SoundCloud"
-            desc="Marcus Billman"
-            href="https://soundcloud.com/xfoxx37"
-            icon="music"
-          ></LinkCard>
-          <LinkCard
-            title="Instagram"
-            desc="marcusbillman"
-            href="https://www.instagram.com/marcusbillman"
-            icon="instagram"
-          ></LinkCard>
+              <BaseButton type="submit">Send message</BaseButton>
+            </form>
+          </div>
         </section>
+        <SocialsSection force-vertical="true" />
       </div>
     </div>
   </div>
@@ -55,79 +40,71 @@
 
 <script setup>
 import BaseButton from '@/components/BaseButton.vue'
+import EmailCard from '@/components/EmailCard.vue'
 import LinkCard from '@/components/LinkCard.vue'
+import SocialsSection from '@/components/SocialsSection.vue'
+import TheHeader from '@/components/TheHeader.vue'
 </script>
 
 <style lang="scss" scoped>
 @use "@/styles/breakpoints" as *;
 @use "@/styles/colours" as *;
 
-p {
-  font-size: 2.4rem;
-  a {
-    color: $blueberry-500;
+.sections {
+  display: flex;
+  flex-direction: column;
+  gap: clamp(10rem, 20rem, 20vh);
+  @include for-tablet-landscape-up {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    gap: 6.4rem 4.8rem;
   }
 }
 
-.page {
-  display: grid;
-}
-
-.contact {
-  display: grid;
-  grid-template-columns: 1fr;
-  gap: 6.4rem 4.8rem;
+.contact-methods {
+  display: flex;
+  flex-direction: column;
+  gap: 6.4rem;
   @include for-tablet-landscape-up {
-    grid-template-columns: repeat(2, 1fr);
+    max-width: 60rem;
+  }
+  &__heading {
+    margin-bottom: 3.2rem;
+  }
+  &__socials {
+    display: flex;
+    flex-direction: column;
+    gap: 2.4rem;
   }
 }
 
 .form {
-  display: grid;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
   gap: 3.2rem;
-  margin-top: 6.4rem;
   &__fields {
-    display: grid;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
     gap: 2.4rem;
   }
-  &__field {
-    display: block;
+  label {
+    font-weight: 500;
   }
-}
-
-label {
-  font-weight: 500;
-}
-
-input,
-textarea {
-  width: 100%;
-  background: $white;
-  border: 2px solid $grey-200;
-  border-radius: 0.8rem;
-  padding: 0.8rem 1.6rem;
-  margin-top: 0.4rem;
-}
-
-textarea {
-  min-height: 20rem;
-  max-height: 100vh;
-  resize: vertical;
-}
-
-.button {
-  @include for-tablet-portrait-up {
-    width: max-content;
+  input,
+  textarea {
+    width: 100%;
+    background: $white;
+    border: 2px solid $grey-200;
+    border-radius: 0.8rem;
+    padding: 0.8rem 1.6rem;
+    margin-top: 0.4rem;
   }
-}
-
-.socials {
-  display: none;
-  @include for-tablet-landscape-up {
-    display: grid;
-    align-content: start;
-    justify-items: end;
-    gap: 2.4rem;
+  textarea {
+    min-height: 20rem;
+    max-height: 100vh;
+    resize: vertical;
   }
 }
 </style>
