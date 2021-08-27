e54e4b4eb7decdc4ec5a78c64fa1fbe2a11e7e74
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-08-27 15:31:58 +0200

    💄 CaseStudyPage: Update to new design

--- web/src/views/CaseStudyPage.vue
+++ web/src/views/CaseStudyPage.vue
@@ -1,74 +1,87 @@
 <template>
   <div class="page">
-    <section v-if="project" class="container header">
-      <h1 class="header__title">{{ project.title }}</h1>
-      <p class="header__description">{{ project.description }}</p>
-    </section>
-    <section v-if="project" class="container container--wide main-image">
-      <img :src="urlFor(project.mainImage)" :alt="project.title" />
-    </section>
-    <section v-if="project" class="container content">
-      <div class="flow body">
-        <SanityBlocks :blocks="project.body" />
-      </div>
-      <div>
-        <div class="data">
-          <div class="data__group">
-            <h3>My roles</h3>
-            <ul class="data__list">
-              <li v-for="roleTag in project.roleTags" :key="roleTag._key">
-                <BaseTag :text="roleTag.name" />
-              </li>
-            </ul>
-          </div>
-          <div class="data__group">
-            <h3>Technologies</h3>
-            <ul class="data__list">
-              <li v-for="techTag in project.techTags" :key="techTag._key">
-                <BaseTag :text="techTag.name" />
-              </li>
-            </ul>
-          </div>
-          <div class="data__group">
-            <h3>Date</h3>
-            <p class="data__date">{{ project.date }}</p>
+    <TheHeader
+      v-if="project"
+      :title="project.title"
+      :description="project.description"
+      transparent="true"
+    />
+    <div class="sections">
+      <section v-if="project" class="main-image section">
+        <BlockImage :asset="project.mainImage" :alt="project.title" />
+      </section>
+      <section v-if="project" class="body section">
+        <SanityBlocks :blocks="project.body" :serializers="serializers" />
+      </section>
+      <section v-if="project?.additionalImages" class="additional-images">
+        <div class="additional-images__container container container--wide">
+          <img
+            v-for="image in project.additionalImages"
+            :key="image._key"
+            class="additional-images__img"
+            :src="urlFor(image)"
+            :alt="image.caption"
+          />
+        </div>
+      </section>
+      <section v-if="project" class="info section">
+        <div class="info__container container">
+          <div class="info__box">
+            <div class="info__data">
+              <div class="info__group">
+                <h3>My roles</h3>
+                <ul class="info__list">
+                  <li v-for="roleTag in project.roleTags" :key="roleTag._key">
+                    <BaseTag :text="roleTag.name" />
+                  </li>
+                </ul>
+              </div>
+              <div class="info__group">
+                <h3>Built with</h3>
+                <ul class="info__list">
+                  <li v-for="techTag in project.techTags" :key="techTag._key">
+                    <BaseTag :text="techTag.name" />
+                  </li>
+                </ul>
+              </div>
+              <div class="info__group">
+                <h3>Project date</h3>
+                <p class="info__date">{{ project.date }}</p>
+              </div>
+            </div>
+            <div class="info__links">
+              <BaseButton
+                :href="project.primaryLink.url"
+                icon="arrow-top-right"
+                >{{ project.primaryLink.text }}</BaseButton
+              >
+              <BaseButton
+                :href="project.secondaryLink.url"
+                icon="arrow-top-right"
+                variant="secondary"
+                >{{ project.secondaryLink.text }}</BaseButton
+              >
+            </div>
           </div>
         </div>
-        <div class="links">
-          <BaseButton :href="project.primaryLink.url" icon="arrow-top-right">{{
-            project.primaryLink.text
-          }}</BaseButton>
-          <BaseButton
-            :href="project.secondaryLink.url"
-            icon="arrow-top-right"
-            variant="secondary"
-            >{{ project.secondaryLink.text }}</BaseButton
-          >
-        </div>
-      </div>
-    </section>
-    <section v-if="project" class="container container--wide additional-images">
-      <img
-        v-for="image in project.additionalImages"
-        :key="image._key"
-        :src="urlFor(image)"
-        :alt="image.caption"
-      />
-    </section>
-    <section class="container next-project">
-      <BaseButton
-        v-if="nextProject"
-        :href="`/portfolio/${nextProject.slug.current}`"
-        icon="arrow-right"
-        >Next project</BaseButton
-      >
-    </section>
+      </section>
+      <section class="next-project section">
+        <BaseButton
+          v-if="nextProject"
+          :href="`/portfolio/${nextProject.slug.current}`"
+          icon="arrow-right"
+          >Next project</BaseButton
+        >
+      </section>
+    </div>
   </div>
 </template>
 
 <script setup>
 import BaseButton from '@/components/BaseButton.vue'
 import BaseTag from '@/components/BaseTag.vue'
+import BlockImage from '@/components/BlockImage.vue'
+import TheHeader from '@/components/TheHeader.vue'
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { useStore } from 'vuex'
@@ -84,86 +97,129 @@ const project = computed(() =>
 const nextProject = computed(() =>
   store.getters.getNextProject(route.params.slug)
 )
+
+const serializers = {
+  types: {
+    image: BlockImage
+  }
+}
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @use "@/styles/breakpoints" as *;
 @use "@/styles/colours" as *;
 
-img {
-  object-fit: cover;
-  border-radius: 1.6rem;
-}
-
-.header {
-  h1 {
-    font-size: clamp(4rem, 6vw, 7.2rem);
-    margin-bottom: 2.4rem;
-  }
-}
-
-.header__description,
-.body {
-  @include for-tablet-landscape-up {
-    font-size: 2.4rem;
-  }
+.sections {
+  display: flex;
+  flex-direction: column;
+  gap: clamp(10rem, 20rem, 20vh);
 }
 
 .main-image {
-  margin-top: 6.4rem;
-  margin-bottom: 6.4rem;
-  @include for-tablet-landscape-up {
-    margin-top: 15rem;
-    margin-bottom: 15rem;
+  position: relative;
+  // Dotted background to replace the one that's normally in the page header
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 66%;
+    width: 100%;
+    height: 200vh;
+    background-image: radial-gradient($grey-200 2px, transparent 0);
+    background-size: 32px 32px;
+    background-position: center top;
+    background-attachment: fixed;
+    z-index: -1;
   }
 }
 
-.content {
-  display: grid;
-  grid-template-columns: 1fr;
-  gap: 6.4rem 4.8rem;
-  margin-bottom: 15rem;
-  @include for-desktop-up {
-    grid-template-columns: repeat(2, 1fr);
+.body {
+  p {
+    padding: 0 1.6rem;
+    margin: 0 auto;
+    & + p {
+      margin-top: 3.2rem;
+    }
+  }
+  .image-block:not(:last-child) {
+    margin: clamp(10rem, 20rem, 20vh) 0;
   }
 }
 
-.data {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
-  gap: 3.2rem 6.4rem;
+.info {
+  &__box {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 6.4rem;
+    @include for-tablet-landscape-up {
+      flex-direction: row;
+      justify-content: space-between;
+      gap: 0;
+      border: 2px solid $grey-200;
+      border-radius: 1.6rem;
+      padding: 3.2rem;
+    }
+  }
+  &__data {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 3.2rem 6.4rem;
+    flex-grow: 1;
+  }
   &__group {
-    display: grid;
+    display: flex;
+    flex-direction: column;
     gap: 1.6rem;
-    height: max-content;
   }
   &__list {
-    display: grid;
+    display: flex;
+    flex-direction: column;
     gap: 1rem;
   }
-}
-
-.links {
-  display: grid;
-  gap: 2rem;
-  margin-top: 4.8rem;
-  @include for-tablet-portrait-up {
-    grid-template-columns: repeat(2, max-content);
+  &__date {
+    line-height: 1;
+  }
+  &__links {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    width: 100%;
+    @include for-tablet-landscape-up {
+      align-items: flex-end;
+      width: auto;
+    }
   }
 }
 
 .additional-images {
-  display: grid;
-  grid-template-columns: repeat(1, minmax(20rem, 1fr));
-  gap: 6.4rem 4.8rem;
-  margin-bottom: 20rem;
-  @include for-tablet-landscape-up {
-    grid-template-columns: repeat(2, minmax(20rem, 1fr));
-    img {
+  &__container {
+    display: grid;
+    grid-template-columns: repeat(1, minmax(20rem, 1fr));
+    gap: 6.4rem 4.8rem;
+    @include for-tablet-landscape-up {
+      grid-template-columns: repeat(2, minmax(20rem, 1fr));
+      padding: 0 clamp(3.2rem, 40rem, 10vw - 2.4rem);
+    }
+  }
+  &__img {
+    position: relative;
+    object-fit: cover;
+    border-radius: 1.6rem;
+    @include for-tablet-landscape-up {
       &:nth-child(even) {
-        transform: translateY(6.4rem);
+        top: 10rem;
+        &:last-child {
+          margin-bottom: 10rem;
+        }
       }
     }
   }
+}
+
+.next-project {
+  display: grid;
+  place-items: center;
 }
 </style>
