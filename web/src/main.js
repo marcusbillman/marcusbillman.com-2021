073e5a79d80f99e7bbde45a431c2aa2ed7e504e3
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-11-22 09:48:33 +0100

    ➖ Web: Remove vue-svg-inline-plugin

--- web/src/main.js
+++ web/src/main.js
@@ -2,12 +2,10 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
-import VueSvgInlinePlugin from 'vue-svg-inline-plugin'
 import { SanityBlocks } from 'sanity-blocks-vue-component'
 
 createApp(App)
   .use(router)
   .use(store)
-  .use(VueSvgInlinePlugin)
   .component('SanityBlocks', SanityBlocks)
   .mount('#app')
